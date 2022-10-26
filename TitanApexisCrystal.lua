f6e46f0cdc7d55e900f927659568f0f6d9449525
Blakenfeder <git@blakenfeder.com>
2022-10-26 01:48:03 -0300

    Update to 100000 and add new features and fixes
     * Set currency count color to red when matching the cap
    
     * Ensure tooltip title color matches currency rarity
    
     * Fix error when currency was not yet discovered

--- TitanApexisCrystal.lua
+++ TitanApexisCrystal.lua
@@ -40,13 +40,23 @@ end
 TitanApexisCrystal.Const.Version = GetAddOnMetadata(TitanApexisCrystal.Const.Name, "Version")
 TitanApexisCrystal.Const.Author = GetAddOnMetadata(TitanApexisCrystal.Const.Name, "Author")
 
--- Text colors
+-- Text colors (AARRGGBB)
 local BKFD_C_BURGUNDY = "|cff993300"
 local BKFD_C_GRAY = "|cff999999"
 local BKFD_C_GREEN = "|cff00ff00"
 local BKFD_C_ORANGE = "|cffff8000"
+local BKFD_C_RED = "|cffff0000"
 local BKFD_C_WHITE = "|cffffffff"
 local BKFD_C_YELLOW = "|cffffcc00"
+
+-- Text item colors (AARRGGBB)
+local BKFD_C_COMMON = "|cffffffff"
+local BKFD_C_UNCOMMON = "|cff1eff00"
+local BKFD_C_RARE = "|cff0070dd"
+local BKFD_C_EPIC = "|cffa335ee"
+local BKFD_C_LEGENDARY = "|cffff8000"
+local BKFD_C_ARTIFACT = "|cffe5cc80"
+local BKFD_C_BLIZZARD = "|cff00ccff"
 
 -- Load Library references
 local LT = LibStub("AceLocale-3.0"):GetLocale("Titan", true)
@@ -56,6 +66,9 @@ local L = LibStub("AceLocale-3.0"):GetLocale(TitanApexisCrystal.Const.Id, true)
 local BKFD_AC_UPDATE_FREQUENCY = 0.0
 local currencyCount = 0.0
 local currencyMaximum
+local seasonalCount = 0.0
+local isSeasonal = false
+local currencyDiscovered = false
 
 function TitanPanelApexisCrystalButton_OnLoad(self)
   self.registry = {
@@ -64,7 +77,7 @@ function TitanPanelApexisCrystalButton_OnLoad(self)
     version = TitanApexisCrystal.Const.Version,
     menuText = L["BKFD_TITAN_AC_MENU_TEXT"], 
     buttonTextFunction = "TitanPanelApexisCrystalButton_GetButtonText",
-    tooltipTitle = L["BKFD_TITAN_AC_TOOLTIP_TITLE"],
+    tooltipTitle = BKFD_C_UNCOMMON..L["BKFD_TITAN_AC_TOOLTIP_TITLE"],
     tooltipTextFunction = "TitanPanelApexisCrystalButton_GetTooltipText",
     icon = "Interface\\Icons\\inv_apexis_draenor",
     iconWidth = 16,
@@ -88,33 +101,57 @@ end
 function TitanPanelApexisCrystalButton_GetButtonText(id)
   local currencyCountText
   if not currencyCount then
-    currencyCountText = "??"
+    currencyCountText = "0"
   else  
     currencyCountText = TitanApexisCrystal.Util_GetFormattedNumber(currencyCount)
+  end
+
+  if (currencyMaximum and not(currencyMaximum == 0) and currencyCount and currencyMaximum == currencyCount) then
+    currencyCountText = BKFD_C_RED..currencyCountText
   end
 
   return L["BKFD_TITAN_AC_BUTTON_LABEL"], TitanUtils_GetHighlightText(currencyCountText)
 end
 
 function TitanPanelApexisCrystalButton_GetTooltipText()
+  if (not currencyDiscovered) then
+    return
+      L["BKFD_TITAN_AC_TOOLTIP_DESCRIPTION"].."\r"..
+      " \r"..
+      TitanUtils_GetHighlightText(L["BKFD_TITAN_AC_TOOLTIP_NOT_YET_DISCOVERED"])
+  end
 
-  local totalLabel = L["BKFD_TITAN_AC_TOOLTIP_COUNT_LABEL"]
+  -- Set which total value will be displayed
+  local tooltipCurrencyCount = currencyCount
+  if (isSeasonal) then
+    tooltipCurrencyCount = seasonalCount
+  end
+
+  -- Set how the total value will be displayed
   local totalValue = string.format(
     "%s/%s",
-    TitanApexisCrystal.Util_GetFormattedNumber(currencyCount),
+    TitanApexisCrystal.Util_GetFormattedNumber(tooltipCurrencyCount),
     TitanApexisCrystal.Util_GetFormattedNumber(currencyMaximum)
   )
-  if(not currencyMaximum or currencyMaximum == 0) then
-    totalLabel = L["BKFD_TITAN_AC_TOOLTIP_COUNT_LABEL_TOTAL"]
+  if (not currencyMaximum or currencyMaximum == 0) then
     totalValue = string.format(
       "%s",
-      TitanApexisCrystal.Util_GetFormattedNumber(currencyCount)
+      TitanApexisCrystal.Util_GetFormattedNumber(tooltipCurrencyCount)
     )
+  elseif (currencyMaximum == tooltipCurrencyCount) then
+    totalValue = BKFD_C_RED..totalValue
+  end
+  
+  local totalLabel = L["BKFD_TITAN_AC_TOOLTIP_COUNT_LABEL_TOTAL_MAXIMUM"]
+  if (isSeasonal) then
+    totalLabel = L["BKFD_TITAN_AC_TOOLTIP_COUNT_LABEL_TOTAL_SEASONAL"]
+  elseif (not currencyMaximum or currencyMaximum == 0) then
+    totalLabel = L["BKFD_TITAN_AC_TOOLTIP_COUNT_LABEL_TOTAL"]
   end
 
   return
     L["BKFD_TITAN_AC_TOOLTIP_DESCRIPTION"].."\r"..
-    "                                                                     \r"..
+    " \r"..
     totalLabel..TitanUtils_GetHighlightText(totalValue)
 end
 
@@ -126,8 +163,11 @@ function TitanPanelApexisCrystalButton_OnUpdate(self, elapsed)
 
     local info = TitanApexisCrystal.GetCurrencyInfo()
     if (info) then
+      currencyDiscovered = true
       currencyCount = tonumber(info.quantity)
       currencyMaximum = tonumber(info.maxQuantity)
+      seasonalCount = tonumber(info.totalEarned)
+      isSeasonal = info.useTotalEarnedForMaxQty
     end
 
     TitanPanelButton_UpdateButton(TitanApexisCrystal.Const.Id)
